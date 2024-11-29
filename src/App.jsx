e8aa2251be3a89de57b3733cb71dd5f6760c3b45
abubaker-coder <bakerdomore@gmail.com>
2024-11-29 21:46:09 +0500

    push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Datapro from './components/Datapro';
+
+function App() {
+  return (
+    <div className="App">
+      <Datapro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Datapro.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import Data from '../Data/Data.json'
+
+const Datapro = () => {
+
+    const [products, setProducts] = useState(Data.products)
+    products.map(prod =>{
+        console.log(prod);
+        
+    })
+        
+  return (
+    <div>
+      <h1>Data</h1>
+    </div>
+  )
+}
+
+export default Datapro
